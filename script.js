0599a03ae72d3f163ad3ca64251b7af61fb98f7e
Arya-Shree9 <aryashahdeo@gmail.com>
2024-07-11 20:28:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      document.body.classList.add('scrolled');
+    } else {
+      document.body.classList.remove('scrolled');
+    }
+  });
+  
